f9a44f21a2f6cc7780473d53acf8e8ed2053d110
Tim Odermatt <tim.odermatt@students.ibaw.ch>
2024-06-05 15:42:14 +0200

    changed localhost urls to render

--- Frontend/src/api/task.js
+++ Frontend/src/api/task.js
@@ -1,9 +1,9 @@
 export const fetchTasks = () => {
-  return fetch("http://localhost:1111/task");
+  return fetch("https://todolistbackend-9aoa.onrender.com/task");
 };
 
 export const updateTaskById = async (id, title) => {
-  return fetch(`http://localhost:1111/task/${id}`, {
+  return fetch(`https://todolistbackend-9aoa.onrender.com/task/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(title),
@@ -11,7 +11,7 @@ export const updateTaskById = async (id, title) => {
 };
 
 export const createTask = async (payload) => {
-  return fetch(`http://localhost:1111/task/`, {
+  return fetch(`https://todolistbackend-9aoa.onrender.com/task/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
@@ -19,7 +19,7 @@ export const createTask = async (payload) => {
 };
 
 export const deleteTaskById = async (id) => {
-  return fetch(`http://localhost:1111/task/${id}`, {
+  return fetch(`https://todolistbackend-9aoa.onrender.com/task/${id}`, {
     method: "DELETE",
   });
 };
